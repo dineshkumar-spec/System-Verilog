bf8593dc4319357dcf0e160d0e16d586df0e980c
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-07 15:50:25 +0530

    Update 04.packed array.sv

--- Day_1/Array/04.packed array.sv
+++ Day_1/Array/04.packed array.sv
@@ -6,3 +6,14 @@ module packedarr;
     end
   end
 endmodule
+//---------------
+module packedarr;
+  logic[3:0][3:0]arr;
+  initial begin
+    arr=16'h48754989;
+    $display("array[%0h]",arr);
+    foreach(arr[i])begin
+      $display("array[%0d]=[%0d]",i,arr[i]);
+    end
+  end
+endmodule
