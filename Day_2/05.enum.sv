45b5284ceb27f452a026719af050588ab9b0c5a4
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-12 18:35:51 +0530

    Create 05.enum.sv

---
+++ Day_2/05.enum.sv
@@ -0,0 +1,17 @@
+module enu;
+  typedef enum{DINESH,KUMAR,VLSI,ENGINEER}job;
+   job a,b;
+    int i;
+  initial begin
+    $display("%s",a.name());
+    a=a.prev();
+    $display("Previous %s",a.name());
+    b=a;
+    $display("%s",b.name());
+    b=job'(a+1);
+    $display("%s",b.name());
+    $display("Last %s",a.name());
+    a=a.next();
+    $display("Next %s",a.name()); 
+  end
+endmodule
