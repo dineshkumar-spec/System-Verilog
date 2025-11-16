745e22ab5e20c9bae19cbc60c00c07cfade85ccb
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-16 18:14:47 +0530

    Create forever.sv

---
+++ Day_5/forever.sv
@@ -0,0 +1,13 @@
+module forever_eg;
+  int count;
+  initial begin
+  forever begin
+    $display("value of count = %0d",count);
+    count++;
+    #5;
+  end
+  end
+  initial begin
+    #50 $finish;
+  end
+endmodule
