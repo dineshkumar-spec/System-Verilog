209bff54d4893367addac084c5656188ef841cb0
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-18 19:08:21 +0530

    Update 03.calling a function in display st.sv

--- Day_7/Function/03.calling a function in display st.sv
+++ Day_7/Function/03.calling a function in display st.sv
@@ -1,13 +1,11 @@
 //calling function in display statement
 module eg;
   function string odd_even ();
-    int n=987;
-  
+    int n=987;  
     if(n%2==0)
       return "EVEN";
     else
       return "ODD";
-
   endfunction
   
   initial begin
