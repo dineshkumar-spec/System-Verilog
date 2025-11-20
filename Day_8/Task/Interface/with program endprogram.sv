0f2eea57253421f64ad2739fa7c122154bdc5436
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-21 00:19:13 +0530

    Create with program endprogram.sv

---
+++ Day_8/Task/Interface/with program endprogram.sv
@@ -0,0 +1,19 @@
+module design_ex(output bit [7:0] addr);
+  initial begin
+    addr<=10;
+    end
+endmodule
+
+program testbench(input bit [7:0] addr);
+  initial begin
+    $display("Addr = %0d",addr);
+  end
+endprogram
+
+module tbench_top;
+  wire [7:0] addr;
+  
+  design_ex dut (addr);
+  testbench test(addr);
+endmodule
+//Addr = 10
