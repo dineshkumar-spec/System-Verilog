e5436077819a46ddcdf88b1c77220df305616ea7
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-23 15:22:50 +0530

    Update 01.string.sv

--- Day_2/string/01.string.sv
+++ Day_2/string/01.string.sv
@@ -10,8 +10,8 @@ module str;
   initial begin
     $display("s1=%s|s2=%s|s3=%s|s4=%s|s5=%s|s6=%s",s1,s2,s3,s4,s5,s6);
     $display(s1.tolower());
-    s2.getc(1);
-    $display("%s",s2);
+    //s2.getc(1);
+    $display("%s",s2.getc(0));
     s3.putc(2,"D");
     $display("%s",s3);
     i=s4.atoi();
@@ -20,3 +20,10 @@ module str;
     $display("%s",s4);
   end
 endmodule
+/*output:
+# s1=HARD|s2=WORK|s3=NEVER|s4=12345FAILS|s5=|s6=HELLO
+# hard
+# W
+# NEDER
+# 12345
+# 12345*/
