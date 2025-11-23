4d447a0ab238f64da1b71e833b7d84a8380deedf
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-23 13:12:06 +0530

    Update 10.Array reduction.sv

--- Day_1/Array/10.Array reduction.sv
+++ Day_1/Array/10.Array reduction.sv
@@ -9,3 +9,20 @@ module arr_reduction;
     end
   end
 endmodule
+-----------------------------------------------------------------
+module arr_reduction;
+  int arr[]='{10,89,6,43,1};
+  int result;
+  initial begin
+    $display("sum is %0d",arr.sum());
+    $display("product is %0d",arr.product());
+    $display("and is %0d",arr.and());
+    $display("or is %0d",arr.or());
+    $display("xor is %0d",arr.xor());
+    foreach(arr[i])begin
+      arr[i]=i;
+      result = arr.sum();
+      $display(" sum is %0d",result);
+    end
+  end
+endmodule
