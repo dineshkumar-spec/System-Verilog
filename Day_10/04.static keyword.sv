40a6978b69500e59bdc6029f7ce25e32f8e87579
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-02 11:37:25 +0530

    Create 04.static keyword.sv

---
+++ Day_10/04.static keyword.sv
@@ -0,0 +1,27 @@
+class packet;
+  //class property
+  byte packet_id;
+  //static property
+  static byte no_of_pkt_created;
+  //constructor
+  function new();
+    no_of_pkt_created++;
+    packet_id = no_of_pkt_created;
+  endfunction
+  //method
+  function void display();
+    $display("-----------------");
+    $display("packet id = %0d",packet_id);
+    $display("-----------------");
+  endfunction
+endclass
+
+module static_property;
+  packet pkt[3];
+  initial begin
+    foreach (pkt[i])begin
+      pkt[i] = new();
+      pkt[i].display();
+    end
+  end
+endmodule
