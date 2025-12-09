bd4dd57db33b56c4efc673dc8dd48ff1d30903b3
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-09 11:14:28 +0530

    Create Dynamic casting.sv

---
+++ Day_14/Dynamic casting.sv
@@ -0,0 +1,27 @@
+class parentclass;
+int addr;
+  function display();
+    $display("value of addr=%0d",addr);
+  endfunction
+endclass
+
+class childclass extends parentclass;
+int data;
+  function display();
+    super.display();
+    $display("value of data=%0d",data);
+  endfunction
+endclass
+
+module casting;
+  initial begin
+    parentclass p = new();
+    childclass c = new();
+    c.addr = 10;
+    c.data=20;
+    p=c;
+    c.display();
+  end
+endmodule
+//# value of addr=10
+//# value of data=20
