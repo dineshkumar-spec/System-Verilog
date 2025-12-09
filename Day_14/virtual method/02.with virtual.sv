0dc7a1a600731bf03465645014a0ef27c3113e77
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-09 12:25:15 +0530

    Create 02.with virtual.sv

---
+++ Day_14/virtual method/02.with virtual.sv
@@ -0,0 +1,27 @@
+class packet;
+int i=1;
+  virtual function integer init();
+  init=i;
+  endfunction
+endclass
+
+class childpacket extends packet;
+int i=2;
+  function integer init();
+  init = -i;
+  endfunction
+endclass
+  
+module class_eg;
+  int j;
+  initial begin
+    childpacket c = new();
+    packet p;
+    p = c;
+    $display("value of i = %0d",p.i);
+    j=p.init();
+    $display("value of j = %0d",j);
+  end
+endmodule
+//value of i = 1
+//value of j = -2
