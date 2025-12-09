2da80aa00d5f38633bfc8423aa301e33747fba5f
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-09 09:49:59 +0530

    Create 07.without super keyword.sv

---
+++ Day_13/Inheritance/07.without super keyword.sv
@@ -0,0 +1,25 @@
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
+class parentclass;
+  bit[31:0]data=1;;
+  function void display();
+  $display("value of data=%0d",data);
+  endfunction
+endclass
+
+class childclass extends parentclass;
+  bit[31:0]data=100;
+  function void display();
+    $display("value of data=%0d",data);
+  endfunction
+endclass
+
+module super_eg;
+initial begin
+  childclass c = new();
+ // c.data=10;
+  c.display();
+end
+endmodule
