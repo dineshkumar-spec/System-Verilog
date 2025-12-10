84dd493de580b41782281e73927faf4e69c718f8
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-10 13:06:11 +0530

    Create 03.without virtual poly.sv

---
+++ Day_15/Polymorphism/03.without virtual poly.sv
@@ -0,0 +1,24 @@
+class packet;
+  int i=1;
+  function int init();
+  init=i;
+  endfunction
+endclass
+
+class child extends packet;
+  int i=2;
+  function int init();
+  init=-i;
+  endfunction
+endclass
+
+module eg;
+ int j;
+  initial begin
+    child c=new();
+    packet p=c;
+    j=p.init();
+    $display("value of j = %0d",j);
+  end
+endmodule
+//value of j = 1
