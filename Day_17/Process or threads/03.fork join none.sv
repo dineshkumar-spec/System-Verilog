2ba75f42a60e480d12c968bbff72a4a179fe0942
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-12 16:42:17 +0530

    Create 03.fork join none.sv

---
+++ Day_17/Process or threads/03.fork join none.sv
@@ -0,0 +1,24 @@
+module fork_join_none;
+  initial begin
+  fork
+    begin
+      $display($time,"\tProcess1 started");
+      #5;
+      $display($time,"\tProcess1 finished");
+    end
+    
+    begin
+      $display($time,"\tProcess2 started");
+      #20;
+      $display($time,"\tProcess2 finished");
+    end
+  join_none
+    $display($time,"\toutside fork join");
+  
+  end
+endmodule
+/* 0	outside fork join
+                   0	Process1 started
+                   0	Process2 started
+                   5	Process1 finished
+                  20	Process2 finished*/
