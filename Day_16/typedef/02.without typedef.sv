84b466a5f8369ed6abd81f207f55275b30c3b035
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-12 16:03:34 +0530

    Create 02.without typedef.sv

---
+++ Day_16/typedef/02.without typedef.sv
@@ -0,0 +1,17 @@
+//typedef class c2;
+class c1;
+  c2 c;
+endclass
+
+class c2;
+  c1 c;
+endclass
+
+module typedef_eg;
+  initial begin
+//     c1 class1;
+//    c2 class2;
+    $display("Inside typedef class");
+  end
+endmodule
+//** Error: testbench.sv(3): Invalid type 'c2'. Please check the type of the variable 'c'.
