a09e03b413a79831e1cf18b1f3a3f647d2239443
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-12 16:34:16 +0530

    Update 02.fork join any.sv

--- Day_17/Process or threads/02.fork join any.sv
+++ Day_17/Process or threads/02.fork join any.sv
@@ -2,23 +2,23 @@ module fork_joinany;
   initial begin
   fork
     begin
-      $display("Process1 started");
+      $display($time,"\tProcess1 started");
       #5;
-      $display("Process1 finished");
+      $display($time,"\tProcess1 finished");
     end
     
     begin
-      $display("Process2 started");
+      $display($time,"\tProcess2 started");
       #20;
-      $display("Process2 finished");
+      $display($time,"\tProcess2 finished");
     end
   join_any
     $display($time,"\toutside fork join");
-    $finish;
+ 
   end
 endmodule
-/* Process1 started
- Process2 started
- Process1 finished
-                    5	outside fork join
-*/
+/*                 0	Process1 started
+                   0	Process2 started
+                   5	Process1 finished
+                   5	outside fork join
+                  20	Process2 finished*/
