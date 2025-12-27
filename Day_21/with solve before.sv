ec536a878a4d3f6678319bace0f600f58ccb27b5
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-27 13:02:05 +0530

    Create with solve before.sv

---
+++ Day_21/with solve before.sv
@@ -0,0 +1,31 @@
+class packet;
+  rand bit a;
+  rand bit[3:0]b;
+  constraint sab {solve a before b;}
+  constraint c1 {(a==1)->b==0;}
+endclass
+
+module solve_before;
+  initial begin
+    packet pkt = new();
+    repeat(15)begin
+      pkt.randomize();
+      $display("a=%0d b=%0d ",pkt.a,pkt.b);
+    end
+  end
+endmodule
+/*a=1 b=0 
+a=0 b=10 
+a=0 b=3 
+a=0 b=5 
+a=1 b=0 
+a=0 b=2 
+a=1 b=0 
+a=0 b=15 
+a=0 b=13 
+a=0 b=5 
+a=0 b=10 
+a=1 b=0 
+a=1 b=0 
+a=0 b=4 
+a=0 b=5*/
