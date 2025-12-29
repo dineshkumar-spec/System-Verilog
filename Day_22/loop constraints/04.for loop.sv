cbd702f87647870051b82d9931667eb41fef6822
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-12-29 12:05:49 +0530

    Create 04.for loop.sv

---
+++ Day_22/loop constraints/04.for loop.sv
@@ -0,0 +1,15 @@
+module eg;
+  int i;
+  initial begin
+    for(i=0;i<8;i++)
+      $display("value of i=%0d",i);
+  end
+endmodule
+/*value of i=0
+value of i=1
+value of i=2
+value of i=3
+value of i=4
+value of i=5
+value of i=6
+value of i=7*/
