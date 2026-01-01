6ca2c56a91ed25144540f6104ecbb6a95d480253
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2026-01-01 16:56:15 +0530

    Create 08.trigger and wait using @.sv

---
+++ Day_24/Interprocess communication/08.trigger and wait using @.sv
@@ -0,0 +1,20 @@
+module eg;
+  event ev_1;
+  initial begin
+  fork
+    begin
+      //process 1
+      $display($time,"\t Triggering the event");
+      ->ev_1;
+    end
+    //process 2
+    begin
+      $display($time,"\twaiting for the event to trigger");
+      @(ev_1.triggered);
+      $display($time,"\tEvent triggered");
+    end
+  join
+  end
+endmodule
+/*                   0	 Triggering the event
+                   0	waiting for the event to trigger*/
